3c9e188a5a38799a9de3dd2240cb172c2e130f91
Talandar <talandar66@protonmail.com>
2026-03-15 00:42:05 +0100

    first commit

---
+++ data-updates.lua
@@ -0,0 +1,32 @@
+local planets = {}
+
+if settings.startup["talandar_2k_planets_aquilo"].value then
+	table.insert(planets, "aquilo")
+end
+if settings.startup["talandar_2k_planets_fulgora"].value then
+	table.insert(planets, "fulgora")
+end
+if settings.startup["talandar_2k_planets_gleba"].value then
+	table.insert(planets, "gleba")
+end
+if settings.startup["talandar_2k_planets_nauvis"].value then
+	table.insert(planets, "nauvis")
+end
+if settings.startup["talandar_2k_planets_vulcanus"].value then
+	table.insert(planets, "vulcanus")
+end
+
+for _, name in pairs(planets) do
+	local prototype = data.raw["space-location"][name] or data.raw["planet"][name]
+	prototype.starmap_icon = "__talandar_2k_planets__/graphics/" .. name .. "2048.png"
+	prototype.starmap_icon_size = 2048
+	prototype.icon = "__talandar_2k_planets__/graphics/" .. name .. "64.png"
+	prototype.icon_size = 64
+
+	local technology = data.raw["technology"]["planet-discovery-" .. name]
+	if technology ~= nil then
+		technology.icons =
+			util.technology_icon_constant_planet("__talandar_2k_planets__/graphics/" .. name .. "256.png")
+		technology.icon_size = 512
+	end
+end
---
+++ settings.lua
@@ -0,0 +1,41 @@
+data:extend({
+	{
+		type = "bool-setting",
+		name = "talandar_2k_planets_aquilo",
+		setting_type = "startup",
+		default_value = true,
+		order = "talandar_2k_planets",
+	},
+
+	{
+		type = "bool-setting",
+		name = "talandar_2k_planets_fulgora",
+		setting_type = "startup",
+		default_value = true,
+		order = "talandar_2k_planets",
+	},
+
+	{
+		type = "bool-setting",
+		name = "talandar_2k_planets_gleba",
+		setting_type = "startup",
+		default_value = true,
+		order = "talandar_2k_planets",
+	},
+
+	{
+		type = "bool-setting",
+		name = "talandar_2k_planets_nauvis",
+		setting_type = "startup",
+		default_value = true,
+		order = "talandar_2k_planets",
+	},
+
+	{
+		type = "bool-setting",
+		name = "talandar_2k_planets_vulcanus",
+		setting_type = "startup",
+		default_value = true,
+		order = "talandar_2k_planets",
+	},
+})
