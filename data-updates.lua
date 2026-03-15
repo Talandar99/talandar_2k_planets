5ccd6a2764eb5d03ac8ba3f930c3a1a1b5582098
Talandar <talandar66@protonmail.com>
2026-03-15 10:27:25 +0100

    added corrundum, secretas and frozeta

--- data-updates.lua
+++ data-updates.lua
@@ -16,6 +16,21 @@ if settings.startup["talandar_2k_planets_vulcanus"].value then
 	table.insert(planets, "vulcanus")
 end
 
+if mods["corrundum"] then
+	if settings.startup["talandar_2k_planets_corrundum"].value then
+		table.insert(planets, "corrundum")
+	end
+end
+
+if mods["secretas"] then
+	if settings.startup["talandar_2k_planets_secretas"].value then
+		table.insert(planets, "secretas")
+	end
+	if settings.startup["talandar_2k_planets_frozeta"].value then
+		table.insert(planets, "frozeta")
+	end
+end
+
 for _, name in pairs(planets) do
 	local prototype = data.raw["space-location"][name] or data.raw["planet"][name]
 	prototype.starmap_icon = "__talandar_2k_planets__/graphics/" .. name .. "2048.png"
--- settings.lua
+++ settings.lua
@@ -39,3 +39,33 @@ data:extend({
 		order = "talandar_2k_planets",
 	},
 })
+if mods["corrundum"] then
+	data:extend({
+		{
+			type = "bool-setting",
+			name = "talandar_2k_planets_corrundum",
+			setting_type = "startup",
+			default_value = true,
+			order = "talandar_2k_planets",
+		},
+	})
+end
+
+if mods["secretas"] then
+	data:extend({
+		{
+			type = "bool-setting",
+			name = "talandar_2k_planets_secretas",
+			setting_type = "startup",
+			default_value = true,
+			order = "talandar_2k_planets",
+		},
+		{
+			type = "bool-setting",
+			name = "talandar_2k_planets_frozeta",
+			setting_type = "startup",
+			default_value = true,
+			order = "talandar_2k_planets",
+		},
+	})
+end
